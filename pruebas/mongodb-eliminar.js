f29d4eeed12cf5c233b85fb9afe17d3e46017e74
Elias <eliasalveal18@gmail.com>
2017-03-05 18:50:33 -0300

    Add delete script

---
+++ pruebas/mongodb-eliminar.js
@@ -0,0 +1,37 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+// var user = {name: 'Elias', age: 20};
+// var {name} = user;
+// console.log(name);
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connect to MongoDB server');
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+  // db.collection('Users').deleteMany({name: 'Carlos'}).then((result) => {
+  //   console.log(result);
+  // });
+  db.collection('Users').findOneAndDelete({
+    _id : ObjectID("58bc86de475371c8bb58f918"
+  )}).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2));
+  });
+
+
+  db.close();
+});
