88497d7e3220eb7cf336f228e858ced29073c49f
Elias <eliasalveal18@gmail.com>
2017-03-05 18:00:20 -0300

    Init commit

---
+++ pruebas/mongodb-buscar.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+// var user = {name: 'Elias', age: 20};
+// var {name} = user;
+// console.log(name);
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connect to MongoDB server');
+
+  // db.collection('Todos').find({completed: true}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+  db.collection('Users').find({name: 'Elias'}).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.close();
+});
