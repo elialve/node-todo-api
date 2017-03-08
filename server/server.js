a74125c08b732d0c6a0de492ddac56d63c47f4a8
Elias <eliasalveal18@gmail.com>
2017-03-08 10:17:39 -0300

    Queries

--- server/server.js
+++ server/server.js
@@ -17,21 +17,30 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
     //console.log(req.body);
     var todo = new Todo({
-      text: req.body.text
+        text: req.body.text
     });
     todo.save().then((docs) => {
-      res.send(docs);
+        res.send(docs);
     }, (e) => {
-      res.status(400).send(e);
+        res.status(400).send(e);
     });
 });
 
-
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    });
+}, (e) => {
+    res.status(400).send(e);
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
+module.exports = {
+    app
+};
 
 
 
